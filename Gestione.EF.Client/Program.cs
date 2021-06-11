5744887195ca9196bfcba5b983d0731f380ca889
Selene Chiara Tucciarone <selene.c.tucciarone@avanade.com>
2021-06-11 09:40:56 +0200

    Start Project

---
+++ Gestione.EF.Client/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gestione.EF.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
--- Gestione.EF.Client/Properties/AssemblyInfo.cs
+++ Gestione.EF.Client/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Gestione.EFClient")]
+[assembly: AssemblyTitle("Gestione.EF.Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Gestione.EFClient")]
+[assembly: AssemblyProduct("Gestione.EF.Client")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7b8c6328-ba64-492e-a337-fd45ce85868e")]
+[assembly: Guid("cb8c80be-f151-4e75-af40-45256001d9b1")]
 
 // Version information for an assembly consists of the following four values:
 //
